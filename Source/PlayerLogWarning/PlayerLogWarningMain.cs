f8129a8c10178feffe55da502a70eda2f86e4418
Vincent Wong <vincentwwy168@gmail.com>
2024-05-08 23:26:28 +0800

    Initial HugsLib commit

---
+++ Source/PlayerLogWarning/PlayerLogWarningMain.cs
@@ -0,0 +1,27 @@
+﻿using HugsLib;
+using Verse;
+
+namespace PlayerLogWarning
+{
+    internal class PlayerLogWarningMain : ModBase
+    {
+        public static string MODSHORTID => "V1024-PLW";
+
+        public override string LogIdentifier => MODSHORTID;
+
+        /// <summary>
+        /// Already includes a space character.
+        /// </summary>
+        public static string MODPREFIX => "[" + MODSHORTID + "] ";
+
+        public static void LogError(string message)
+        {
+            Log.Error(MODPREFIX + " " + message);
+        }
+
+        public static void LogWarning(string message)
+        {
+            Log.Warning(MODPREFIX + " " + message);
+        }
+    }
+}
