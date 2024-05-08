d026e52613514b5516ece432a664a55d2874b790
Vincent Wong <vincentwwy168@gmail.com>
2024-05-08 23:43:06 +0800

    Listen to game-start/game-load events

---
+++ Source/PlayerLogWarning/PostFix_Game_InitNewGame.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using PlayerLogWarning;
+using Verse;
+
+namespace EBF.Patches
+{
+    [HarmonyPatch(typeof(Game))]
+    [HarmonyPatch(nameof(Game.InitNewGame))]
+    public class PostFix_Game_InitNewGame
+    {
+        [HarmonyPostfix]
+        public static void PostFix()
+        {
+            LongEventHandler.ExecuteWhenFinished(delegate
+            {
+                LogFileChecker.CheckOnce();
+            });
+        }
+    }
+}
---
+++ Source/PlayerLogWarning/PostFix_Game_LoadGame.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using PlayerLogWarning;
+using Verse;
+
+namespace EBF.Patches
+{
+    [HarmonyPatch(typeof(Game))]
+    [HarmonyPatch(nameof(Game.LoadGame))]
+    public class PostFix_Game_LoadGame
+    {
+        [HarmonyPostfix]
+        public static void PostFix()
+        {
+            LongEventHandler.ExecuteWhenFinished(delegate
+            {
+                LogFileChecker.CheckOnce();
+            });
+        }
+    }
+}
