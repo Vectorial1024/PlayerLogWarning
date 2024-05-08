2845509dd4b48eb86dbf7a67d21d74510d441fe9
Vincent Wong <vincentwwy168@gmail.com>
2024-05-08 23:14:24 +0800

    Initial feature commit
    Also upgrade to SDK style

---
+++ Source/PlayerLogWarning/PlayerLogWarningMod.cs
@@ -0,0 +1,12 @@
+﻿using Verse;
+
+namespace PlayerLogWarning
+{
+    public class PlayerLogWarningMod : Mod
+    {
+        public PlayerLogWarningMod(ModContentPack content) : base(content)
+        {
+            // not much going on here...
+        }
+    }
+}
